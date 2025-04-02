3fd43f556c6d207c86941db2ad9745007efb6efb
Zhimin Wang <fireflymikasa@gmail.com>
2025-04-02 11:34:52 +1100

    update

---
+++ FrontEnd/src/components/QuizOverviewSection.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="quiz-overview-container">
+      <h2 class="section-title">Foster Digital Citizenship</h2>
+      <p class="section-subtitle">
+        Work together to create a safe and better online world.
+      </p>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    name: "QuizOverviewSection"
+  };
+  </script>
+  
+  <style scoped>
+  .quiz-overview-container {
+    padding: 80px 10vw 40px;
+    text-align: center;
+    background-color: #ffffff; /* 暂时纯白背景 */
+  }
+  
+  .section-title {
+    font-size: 32px;
+    font-weight: 700;
+    color: #050C26;
+    margin-bottom: 12px;
+  }
+  
+  .section-subtitle {
+    font-size: 16px;
+    color: #6e6e6e;
+  }
+  </style>
+  
