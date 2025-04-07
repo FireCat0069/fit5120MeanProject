745f8e3bbecbe54cb241db2fd027fe506448c0c5
Zhimin Wang <fireflymikasa@gmail.com>
2025-04-07 21:07:27 +1000

    update for vue
    现在不会再强行锁两屏高了

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -31,8 +31,9 @@ export default {
 html, body {
   margin: 0;
   padding: 0;
-  width: 100vw;
-  height: 200vh; /* 页面高度手动设置为两屏 */
+  width: 100%;
+  min-height: 100vh;
+  /*height: 200vh;  页面高度手动设置为两屏 */
   overflow-x: hidden; /* 禁止横向滚动 */
   overflow-y: auto;   /* 启用垂直滚动 */
   background-color: #FDF8EE;
