a1cc6ad5b4c22c28be255e4ac31c56eda50942ed
XuanhuiWu1103 <153825845+StartAt2023@users.noreply.github.com>
2025-04-10 17:17:51 +1000

    quiz bank post API

--- BackEnd/src/routes/quizQuestionRoutes.js
+++ BackEnd/src/routes/quizQuestionRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAllQuizQuestions } = require('../controllers/quizQuestionController');
+const quizQuestionController = require('../controllers/quizQuestionController');
 
-router.get('/', getAllQuizQuestions);
+// 获取所有问题
+router.get('/questions', quizQuestionController.getAllQuizQuestions);
+
+// 校验答案
+router.post('/validate-answers', quizQuestionController.validateQuizAnswers);
 
 module.exports = router;
